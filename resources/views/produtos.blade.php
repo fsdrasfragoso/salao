04e562af4b9da230092709a32f2e04b9edd4232b
fsdrasfragoso <esdrasfragoso@gmail.com>
2018-12-18 17:17:41 -0300

    produto

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -5,7 +5,7 @@
     <div class="card-body">
         <h5 class="card-title">Cadastro de Produtos</h5>
 
-@if(count($cats) > 0)
+
         <table class="table table-ordered table-hover">
             <thead>
                 <tr>
@@ -19,22 +19,13 @@
                 </tr>
             </thead>
             <tbody>
-    @foreach($cats as $cat)
-                <tr>
-                    <td>{{$cat->id}}</td>
-                    <td>{{$cat->nome}}</td>
-                    <td>
-                        <a href="/categorias/editar/{{$cat->id}}" class="btn btn-sm btn-primary">Editar</a>
-                        <a href="/categorias/apagar/{{$cat->id}}" class="btn btn-sm btn-danger">Apagar</a>
-                    </td>
-                </tr>
-    @endforeach                
+   
             </tbody>
         </table>
-@endif        
+     
     </div>
     <div class="card-footer">
-        <a href="/categorias/novo" class="btn btn-sm btn-primary" role="button">Nova categoria</a>
+        <button class="btn btn-sm btn-primary" role="button">Nova produto</button>
     </div>
 </div>
 
