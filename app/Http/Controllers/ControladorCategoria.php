ba8190c654b48da38e0c45374f166db103ee6cfc
fsdrasfragoso <esdrasfragoso@gmail.com>
2019-01-04 16:13:03 -0300

    cio

--- app/Http/Controllers/ControladorCategoria.php
+++ app/Http/Controllers/ControladorCategoria.php
@@ -8,10 +8,10 @@ use App\Categoria;
 
 class ControladorCategoria extends Controller
 {
-    public function __construct()
+    /* public function __construct()
     {
         $this->middleware('auth');
-    }
+    }   */
     
     
     /**
